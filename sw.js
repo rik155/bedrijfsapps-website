7e70934faaa254572e819a98182b066c19793a48
rik155 <Fokkerrik@gmail.com>
2026-09-16 09:58:14 +0200

    Refresh launcher cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jan-bos-apps-v2';
+const CACHE='jan-bos-apps-v3';
 const SHELL=['/','/style.css','/responsive.css','/app.webmanifest','/janbos-logo.jpg','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
