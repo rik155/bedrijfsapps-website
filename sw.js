5e303de2374d27fa9f5c57696e5d18bc541bb4b4
rik155 <Fokkerrik@gmail.com>
2026-09-18 13:06:45 +0200

    Vernieuw cache voor terugknop

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jan-bos-apps-v9';
+const CACHE='jan-bos-apps-v10';
 const SHELL=['/','/kantoor.html','/open.html','/style.css','/responsive.css','/app.webmanifest','/kantoor.webmanifest','/janbos-logo.png','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
